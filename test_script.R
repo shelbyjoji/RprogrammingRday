8bf1e03db1a2c78398e6f78246d36ed541b09be7
Shelby Joji <shelby021@gmail.com>
2018-09-06 23:43:18 -0400

    first commit

---
+++ test_script.R
@@ -0,0 +1,21 @@
+mat1<-matrix(c(0,6,0,2,7,2,5,0,5),3,3)
+
+mat2<-matrix(NA,3,3)
+
+for(j in 1:3){
+  
+  dataN<-mat1[,j]
+  
+  for(i in 1:3){
+    
+    one<-(dataN[[i]])^2
+    
+    mat2[i,j]<-one
+    
+  }
+  
+}
+
+mat1
+
+mat2
