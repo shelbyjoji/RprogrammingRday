612f36508044ccc8a12a940327d5b2be93283e49
Shelby Joji <shelby021@gmail.com>
2018-10-01 11:32:16 -0400

    new commit 10/01

--- test_script.R
+++ test_script.R
@@ -1,22 +1,2 @@
-mat1<-matrix(c(0,6,0,2,7,2,5,0,5),3,3)
-
-mat2<-matrix(NA,3,3)
-
-for(j in 1:3){
-  
-  dataN<-mat1[,j]
-  
-  for(i in 1:3){
-    
-    one<-(dataN[[i]])^2
-    
-    mat2[i,j]<-one
-    
-  }
-  
-}
-
-mat1
-
-mat2
-mat1*mat2
+d1<-read.csv("C:/Users/HOMEPCUSER/Desktop/Fall 2018/R programming/RprogrammingRday/train_labels.csv")
+d2<-read.csv("C:/Users/HOMEPCUSER/Desktop/Fall 2018/R programming/RprogrammingRday/train_values.csv")
