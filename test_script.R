01ad40f2173bab04a4c9d149c4710c9f5101030b
Shelby Joji <shelby021@gmail.com>
2018-10-01 13:31:53 -0400

    commit_update_10_1_18::1_31

--- test_script.R
+++ test_script.R
@@ -11,23 +11,64 @@ View(train)
 str(train)
 summary(train)
 
-#Exploratory Analysis
-#Age
-#Sex
-#Rest_BP 
-#thal (shows how well blood flows into your heart while exercising or at rest)
-#max_heart_rate_achieved
-#slope_of_peak_exercise_st_segment 
-#oldpeak_eq_st_depression
-#slope 
 
-# Age (continuous numeric variables) vs heart disease(binary)
+#_______________________________
+#Categorical
+#_______________________________
+#Sex
+
+#thal (shows how well blood flows into your heart while exercising or at rest)
+        #results of thallium stress test measuring blood flow to the heart, with possible 
+        #values normal,fixed_defect, reversible_defect
+#chest_pain_type
+        #-- Value 1: typical angina 
+        #-- Value 2: atypical angina 
+        #-- Value 3: non-anginal pain 
+        #-- Value 4: asymptomatic 
+
+
+ 
+#resting_ekg_results
+        #-- Value 0: normal 
+        #-- Value 1: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV) 
+        #-- Value 2: showing probable or definite left ventricular hypertrophy by Estes' criteria 
+
+#slope_of_peak_exercise_st_segment 
+        #-- Value 1: upsloping 
+        #-- Value 2: flat 
+        #-- Value 3: downsloping
+
+#_______________________________
+#Continuous numeric variables
+#_______________________________
+
+#serum_cholesterol_mg_per_dl (126 to 564) 
+#oldpeak_eq_st_depression (ST depression induced by exercise relative to rest )
+#exercise_induced_angina (1 = yes; 0 = no)
+#max_heart_rate_achieved
+#Rest_BP 
+#Age
+
+#heart_disease_present - what we are trying to predict
+
+
+#************************************************************************************************************
+#Exploratory Analysis
+#------------------------------------------------------------------------------------------------------------
 library(ggplot2)
+
+# Custom Binning. I can just give the size of the bin
+ggplot(train, aes(x=age)) + geom_histogram(binwidth = 5,color="white", fill=rgb(0.2,0.7,0.1,0.4) )
+ggplot(train, aes(x=age)) + geom_histogram(binwidth = 5,aes(fill = ..count..)  )
+
+# Age (continuous numeric variable) vs heart disease(binary)
 ggplot(train, aes(age, fill = factor(heart_disease_present))) + 
   geom_histogram(bins=30) + 
   xlab("Age") +theme_few() + scale_colour_few()+
   scale_fill_discrete(name = "Presence_of_Heart_disease") + 
   ggtitle("Age vs Heart disease")
+
+
 
 #Sex(nominal qualitative variable) vs vs heart disease(binary)
 ggplot(train, aes(sex, fill = factor(heart_disease_present))) + 
@@ -37,7 +78,7 @@ ggplot(train, aes(sex, fill = factor(heart_disease_present))) +
   scale_fill_discrete(name = "Presence_of_Heart_disease") + 
   ggtitle("Sex vs Heart disease")
 
-#Sex vs Survived vs Age 
+#Sex vs heart disease vs Age 
 ggplot(train, aes(age, fill = factor(heart_disease_present))) + 
   geom_histogram(bins=30) + 
   xlab("Age") +
@@ -52,5 +93,8 @@ ggplot(train, aes(resting_blood_pressure, fill = factor(heart_disease_present)))
   xlab("Resting_BP")+ 
   scale_fill_discrete(name = "Presence_of_Heart_disease") + 
   ggtitle("Resting_BP vs Heart disease")
+
+#... more exploratory analysis required
+
 
 
